902f16a3325b82bdc669450662a4078b4b73fc95
snayan <snayan@sina.com>
2016-07-12 10:15:04 +0800

    add fork repos config ,add repos sort

--- server/controller/api.controller.js
+++ server/controller/api.controller.js
@@ -7,6 +7,7 @@ var blog=require('../models/blog.model');
 var Q=require('q');
 var _=require('lodash');
 var util=require('../util');
+var config=require('../util/config');
 
 
 // get all tags
@@ -28,26 +29,40 @@ exports.getAllTag=function(req,res,next){
 };
 
 //  get my git project
-exports.getPublicGit=function(req,res,next){
+exports.getPublicGit=function(req,res,next) {
     // var url="https://api.github.com/users/snayan/repos";
-    var repos=[];
-    var option={
-        hostname:'api.github.com',
-        path:'/users/snayan/repos',
+    var repos = [], showGitForks = config.showGitFork;
+    var option = {
+        hostname: 'api.github.com',
+        path: '/users/snayan/repos',
         headers: {
             'User-Agent': 'snayan'
         }
     };
-    var req=https.get(option,function(response) {
+    var req = https.get(option, function (response) {
         // console.log(res);
         response.on('data', function (chunk) {
             repos.push(chunk);
         });
         response.on('end', function () {
-            res.status(200).send(Buffer.concat(repos).toString());
+            var data = JSON.parse(Buffer.concat(repos).toString());
+            repos.length = 0;
+            data.forEach(function (git) {
+                if (!git.private && (config.showGitFork || !git.fork)) {
+                    repos.push(_.pick(git, ['id', 'name', 'full_name', 'private', 'html_url', 'fork', 'forks_count', 'stargazers_count', 'description', 'created_at', 'updated_at', 'pushed_at', 'homepage', 'size', 'language']));
+                }
+            });
+            repos.sort(function (gita, gitb) {
+                var b = gita.fork === gitb.fork;
+                if (b) {
+                    return new Date(gitb.pushed_at).getTime() - new Date(gita.pushed_at).getTime();
+                }
+                return gita.fork - gitb.fork;
+            });
+            res.status(200).json(repos);
         });
     });
-    req.on('error',function(err){
+    req.on('error', function (err) {
         util.logError(err);
         res.status(500).send(err);
     });
--- server/util/config.js
+++ server/util/config.js
@@ -5,25 +5,26 @@
 var path=require('path');
 
 //系统配置信息
-var config={
-    mongodb:{
-        uri:'mongodb://localhost/myblogs',
-        options:{
-            db:{
-                safe:true
+var config= {
+    mongodb: {
+        uri: 'mongodb://localhost/myblogs',
+        options: {
+            db: {
+                safe: true
             }
         }
     },
-    test:{
-        uri:path.join(__dirname,'../../test'),
-        if:true
+    test: {
+        uri: path.join(__dirname, '../../test'),
+        if: true
     },
-    logpath:path.join(__dirname,'../logs/log.txt'),
-    express:{
-        port:3000,
-        host:'localhost'
+    logpath: path.join(__dirname, '../logs/log.txt'),
+    express: {
+        port: 3000,
+        host: 'localhost'
     },
-    root:path.normalize(__dirname+'/../..')
+    root: path.normalize(__dirname + '/../..'),
+    showGitFork: true
 };
 
 module.exports=config;
